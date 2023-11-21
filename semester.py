f88738c308822303628a293887d756c0c23b1d78
anundjore <110898996+anundjore@users.noreply.github.com>
2023-11-21 20:48:00 +0100

    Add files via upload

---
+++ semester.py
@@ -0,0 +1,46 @@
+from collections import defaultdict
+
+## Program laget for å hente ut datoer fra .TSV filen med svar på arrangementer for IFI-navet
+## Legg til semester.py og .TSV filen i samme mappe, og gi filen navn 'skjema.tsv', eller endre variabelnavnet
+## Laget av ÅKJ, bedriftskontakt Høsten 2023.
+
+
+def main(csv):
+    file = open(csv)
+    dictionary_date_and_companies = defaultdict(list)
+    toggle = False
+
+    for line in file:
+        line = line.split("\t")
+
+        dates = line[13].split(",")
+        company = line[2]
+
+        if toggle:
+            for date in dates:
+                dictionary_date_and_companies[date].append(company)
+        toggle = True
+
+    new_list = dictionary_date_and_companies
+
+    file_path = "output.txt"
+
+    with open(file_path, "w") as file:
+        for key, value in new_list.items():
+            file.write(f"{key}: {value}\n")
+
+    print(
+        """
+    _   _                 _    
+    | \ | | __ ___   _____| |_  
+    |  \| |/ _` \ \ / / _ \ __| 
+    | |\  | (_| |\ V /  __/ |_  
+    |_| \_|\__,_| \_/ \___|\__| 
+        """
+    )
+
+    print("Semesterplanen er lagret som output.txt")
+
+
+if __name__ == "__main__":
+    main("skjema.tsv")
